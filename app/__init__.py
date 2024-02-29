ae4037c6133a55fe8e99e5e196ef9708591fb08a
Kevin Marville <kevinmarville@gmail.com>
2024-02-29 16:55:43 +0100

    Update email configuration

--- app/__init__.py
+++ app/__init__.py
@@ -38,7 +38,7 @@ def create_app(config_class=Config):
     app.config['MAIL_SERVER'] = 'smtp.example.com'
     app.config['MAIL_PORT'] = 587
     app.config['MAIL_USE_TLS'] = True
-    app.config['MAIL_USERNAME'] = 'noreply@yourdomain.com'
+    app.config['MAIL_USERNAME'] = 'code@kvnbbg-creations.io'
     # Securely generate a random password for the mail account
     app.config['MAIL_PASSWORD'] = secrets.token_urlsafe(16)
 
