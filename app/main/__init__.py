f9cd0533e1a05b7573f73e153f4a6ce82f93f708
Kevin Marville <kevinmarville@gmail.com>
2024-03-02 05:12:07 +0100

    Add Flask environment variable for development

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,4 +1,7 @@
 # main/__init__.py
 from flask import Blueprint
+import os
 
+# This is the entry point for the application. It creates an instance of the Flask application and runs it.
+os.environ["FLASK_ENV"] = "development"
 main = Blueprint("main", __name__)
