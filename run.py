dd590db44c47febb98935d087314c9327bf2c096
Kevin Marville <kevinmarville@gmail.com>
2024-03-02 05:03:18 +0100

    Update Flask environment configuration

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
+# This Python script is the entry point for a Flask application. Here's a breakdown of what it does:
 from app import create_app
+import os
+
+# This is the entry point for the application. It creates an instance of the Flask application and runs it.
+os.environ["FLASK_ENV"] = "development"
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
