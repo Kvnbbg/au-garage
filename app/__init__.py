7376d51b130ba052d63aa5bd9b3aea18cd80b53e
Kevin Marville <kevinmarville@gmail.com>
2024-04-30 01:08:14 +0200

    Refactor import order and update login_manager configuration in __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,16 @@
 # app/__init__.py
 import os
+
 from flask import Flask
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_wtf.csrf import CSRFProtect
 
-from .database import init_db, get_db_connection
 from config import DevelopmentConfig, ProductionConfig, TestingConfig
 
-__all__ = ['get_db_connection']
+from .database import get_db_connection, init_db
+
+__all__ = ["get_db_connection"]
 
 # Create extension instances
 login_manager = LoginManager()
@@ -23,7 +25,7 @@ def create_app():
     config_class = {
         "development": DevelopmentConfig,
         "testing": TestingConfig,
-        "production": ProductionConfig
+        "production": ProductionConfig,
     }.get(os.environ.get("FLASK_ENV"), DevelopmentConfig)
 
     app.config.from_object(config_class)
@@ -34,21 +36,25 @@ def create_app():
     csrf.init_app(app)  # Initialize CSRF protection
 
     # Setup for Flask-Login
-    login_manager.login_view = "auth.login"  # Specify the view function that handles logins
+    login_manager.login_view = (
+        "auth.login"  # Specify the view function that handles logins
+    )
     login_manager.login_message_category = "info"
 
     # User loader function for Flask-Login
     from app.models import User  # Import here to avoid circular imports
+
     @login_manager.user_loader
     def load_user(user_id):
-        return User.query.get(int(user_id)) # Returns User object or None if not found
-
+        return User.query.get(int(user_id))  # Returns User object or None if not found
 
     # Register blueprints
     from .auth.routes import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint, url_prefix='/auth')
-    
+
+    app.register_blueprint(auth_blueprint, url_prefix="/auth")
+
     from .main.routes import main as main_blueprint
+
     app.register_blueprint(main_blueprint)
     init_db()  # Initialize the database
 
